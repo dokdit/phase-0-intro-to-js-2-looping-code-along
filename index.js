5202a4eee8d5bb23d6805879950bfe794718742a
AK <akueiminyang@cmail.carleton.ca>
2023-03-01 05:51:14 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names,gift){
+    const thanks = []
+    for (let i = 0; i < names.length; i++){
+        let note = `Thank you, ${names[i]}, for the wonderful ${gift} gift!`
+        thanks.push(note)
+    }
+    return thanks
+}
+
+function countDown(number){
+    for (let i = number; i >=0; i--){
+        console.log(i)
+    }
+}
